66ac2f74eb9706448b4b5ec880cbe806ca7dde51
Bolaji Fatade <bjfatade@gmail.com>
2016-07-03 17:55:53 -0400

    first attempt at lru

--- lru.c
+++ lru.c
@@ -12,6 +12,17 @@ extern int debug;
 
 extern struct frame *coremap;
 
+int counter = 0;
+
+
+//algo. Use a global counter
+//add a 'counter' field to the frame struct.
+//evict the frame in coremap that has the lowest counter field
+
+//on each reference, increment counter.
+//on each reference, go to coremap[p->frame].counter and set it to counter
+
+
 /* Page to evict is chosen using the accurate LRU algorithm.
  * Returns the page frame number (which is also the index in the coremap)
  * for the page that is to be evicted.
@@ -19,7 +30,7 @@ extern struct frame *coremap;
 
 int lru_evict() {
 	
-	return 0;
+	return find_min();
 }
 
 /* This function is called on each access to a page to update any information
@@ -27,7 +38,8 @@ int lru_evict() {
  * Input: The page table entry for the page that is being accessed.
  */
 void lru_ref(pgtbl_entry_t *p) {
-
+	counter++;
+	coremap[p->frame].lru_counter = counter;
 	return;
 }
 
@@ -37,3 +49,27 @@ void lru_ref(pgtbl_entry_t *p) {
  */
 void lru_init() {
 }
+
+int find_min(){
+	int i, min, index;
+
+	min = coremap[0].counter;
+	index = 0;
+
+	for(i=1, i<memsize, i++){
+		if (coremap[i].counter < min){
+			index = i;
+			min = coremap[i].counter;
+		}
+	}
+
+	return index;
+}
+
+
+
+
+
+
+
+
