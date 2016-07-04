b211923991cb559bc3612e47d98637ad40c0e982
Bolaji Fatade <bjfatade@gmail.com>
2016-07-04 13:15:41 -0400

    First attempt at clock

--- clock.c
+++ clock.c
@@ -12,14 +12,24 @@ extern int debug;
 
 extern struct frame *coremap;
 
+int clock[memsize];
+int hand = 0;
+
 /* Page to evict is chosen using the clock algorithm.
  * Returns the page frame number (which is also the index in the coremap)
  * for the page that is to be evicted.
  */
 
 int clock_evict() {
+
+	while(clock[hand]!=0){
+
+		clock[hand] = 0;
+		hand++;
+		hand = hand % memsize;
+	}
 	
-	return 0;
+	return hand;
 }
 
 /* This function is called on each access to a page to update any information
@@ -27,6 +37,7 @@ int clock_evict() {
  * Input: The page table entry for the page that is being accessed.
  */
 void clock_ref(pgtbl_entry_t *p) {
+	clock[p->frame >> PAGE_SHIFT] = 1; //get the frame (in regular ints)
 
 	return;
 }
@@ -35,4 +46,8 @@ void clock_ref(pgtbl_entry_t *p) {
  * algorithm. 
  */
 void clock_init() {
+	int i = 0;
+	for (i=0; i<memsize, i++){
+		clock[i] = 0;
+	}
 }
